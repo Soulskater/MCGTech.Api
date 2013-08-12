8d786d065ebc77b9cf233eead5900ef9775829a1
Soulskater <gmeszaros007@gmail.com>
2013-08-12 11:10:50 +0200

    Initial commit

---
+++ HomeSite.Dal/Domain/Forum.cs
@@ -0,0 +1,20 @@
+﻿using Iesi.Collections.Generic;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSite.Dal.Domain
+{
+    public class Forum
+    {
+        public virtual int ForumID { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string Description { get; set; }
+        public virtual DateTime Created { get; set; }
+        public virtual User Moderator { get; set; }
+        public virtual string Identifier { get; set; }
+        private ISet<ForumPost> _posts = new HashedSet<ForumPost>();
+        public virtual ISet<ForumPost> Posts { get { return _posts; } set { _posts = value; } }
+    }
+}
---
+++ HomeSite.Dal/NHibernateHelper.cs
@@ -0,0 +1,37 @@
+﻿using HomeSite.Dal.Domain;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSite.Dal
+{
+    public class NHibernateHelper
+    {
+        private static ISessionFactory _sessionFactory;
+
+        private static ISessionFactory SessionFactory
+        {
+            get
+            {
+                if (_sessionFactory == null)
+                {
+                    var configuration = new Configuration();
+                    configuration.Configure();
+                    configuration.AddAssembly(typeof(Forum).Assembly);
+                    _sessionFactory = configuration.BuildSessionFactory();
+                }
+                return _sessionFactory;
+            }
+        }
+
+        public static ISession OpenSession()
+        {
+            return SessionFactory.OpenSession();
+        }
+    }
+}
