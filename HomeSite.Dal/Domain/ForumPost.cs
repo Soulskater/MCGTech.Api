b92a4b0c8149350306eb4818d4d5df109955e90c
Soulskater <gmeszaros007@gmail.com>
2013-08-12 11:27:06 +0200

    Add commit

---
+++ HomeSite.Dal/Domain/ForumPost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSite.Dal.Domain
+{
+    public class ForumPost
+    {
+        public virtual int ForumPostID { get; set; }
+        public virtual string PostText { get; set; }
+        public virtual Forum Forum { get; set; }
+        public virtual DateTime Created { get; set; }
+        public virtual User User { get; set; }
+    }
+}
---
+++ HomeSite.Dal/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSite.Dal
+{
+    public interface IRepository<T>
+    {
+        void Add(T item);
+    }
+}
